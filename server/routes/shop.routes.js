a3c0f929842da47a24a630af7c7b0b168c7e87fb
BlueOrang3 <matealtunashvili@gmail.com>
2023-05-05 01:39:33 +0400

    feat(shop): shop api and model on server side

---
+++ server/routes/shop.routes.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import authCtrl from '../controllers/auth.controller';
+import userCtrl from '../controllers/user.controller';
+import shopCtrl from '../controllers/shop.controller';
+
+const router = express.Router();
+
+router
+  .route('/api/shops/by/:userId')
+  .post(
+    authCtrl.requireSignin,
+    authCtrl.hasAuthorization,
+    userCtrl.isSeller,
+    shopCtrl.create
+  );
+router.route('/api/shops').get(shopCtrl.list);
+router
+  .route('/api/shops/by/:userId')
+  .get(authCtrl.requireSignin, authCtrl.hasAuthorization, shopCtrl.listByOwner);
+router.route('/api/shop/:shopId').get(shopCtrl.read);
+router
+  .route('/api/shops/:shopId')
+  .put(authCtrl.requireSignin, shopCtrl.isOwner, shopCtrl.update);
+
+router
+  .route('/api/shops/:shopId')
+  .delete(authCtrl.requireSignin, shopCtrl.isOwner, shopCtrl.remove);
+
+router.param('shopId', shopCtrl.shopByID);
+router.param('userId', userCtrl.userByID);
+
+export default router;
